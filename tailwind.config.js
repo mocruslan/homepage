9599fa86a46e86095b01e20c2e3869361c9282e1
Ruslan Mochulskyy <mochulskyy.ruslan@gmail.com>
2023-05-20 18:20:48 +0200

    Added integration of tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				// Light colors
+				'primary-light': '#F7F8FC',
+				'secondary-light': '#FFFFFF',
+				'ternary-light': '#f6f7f8',
+
+				// Dark colors
+				'primary-dark': '#0D2438',
+				'secondary-dark': '#102D44',
+				'ternary-dark': '#1E3851',
+
+				// Extended v3 color
+				gray: colors.neutral,
+			},
+			container: {
+				padding: {
+					DEFAULT: '1rem',
+					sm: '2rem',
+					lg: '5rem',
+					xl: '6rem',
+					'2xl': '8rem',
+				},
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/forms')],
+};
